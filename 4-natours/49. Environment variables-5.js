7f4e0e2828c8e8aa35a07462b303eb3de03ef898
Ashish Kumar Singh <ashishsingh4job@gmail.com>
2022-05-19 14:45:34 +0530

    Update 49. Environment variables-5.js

--- 4-natours/49. Environment variables-5.js
+++ 4-natours/49. Environment variables-5.js
@@ -13,17 +13,16 @@ const userRouter = require("./routes/userRoutes");
 
 // 1. MIDDLEWARES
 
-//We have logger middleware(morgan) here and we want to only run that middleware so to only define it when we are actually in development, so that the login does not happen when the app is in production.
+//We have logger middleware(morgan) here and we want to only run that middleware so to only define it when we are actually in development, so that the logging does not happen when the app is in production.
 if (process.env.NODE_ENV === "developement") {
   app.use(morgan("dev"));
 }
 
-// Why we actually have access to this environment variable here when we didn't really define them in this file but in server.js?
+// Why we actually have access to this environment variable here when we didn't really define them in this file but in 'server.js'?
 
 //That is because the reading of the variables from the file which happens here to the node process only needs to happen once. It's then in the process, and the process is of course the same no matter in what file we are. So we're always in the same process and the environment variables are on the process. So this is available to us in every single file in the project.
 
 app.use(express.json());
-
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
@@ -41,9 +40,3 @@ app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
 module.exports = app;
-
-/*
-
-
-
-*/
